2ed04c0c0a52eb7ad487f80b6d05bd9ffc17c478
holm / Christian Müller <holm@z6c.me>
2021-11-22 23:28:31 +0100

    houesekeeping

---
+++ holm1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="holm1" tilewidth="32" tileheight="32" tilecount="512" columns="16">
+ <image source="../../Docker/Privat/docker1.pve21.z6c.me/nerdraum.world/maps/Tuto/tilesets/outdoor/plants.png" width="512" height="1024"/>
+</tileset>
